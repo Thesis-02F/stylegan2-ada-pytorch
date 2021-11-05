5c8d9dabe882fa873e95eb08ac107dfd9ecb47c3
IsmailShaheen <i.shaheen@aucegypt.edu>
2021-11-06 01:06:32 +0200

    TextDataset added and tested #1

--- training/dataset.py
+++ training/dataset.py
@@ -7,17 +7,27 @@
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
 import os
+import sys
 import numpy as np
+import pandas as pd
 import zipfile
 import PIL.Image
 import json
 import torch
 import dnnlib
 
+from nltk.tokenize import RegexpTokenizer
+from collections import defaultdict
+
 try:
     import pyspng
 except ImportError:
     pyspng = None
+
+if sys.version_info[0] == 2:
+    import cPickle as pickle
+else:
+    import pickle
 
 #----------------------------------------------------------------------------
 
@@ -70,6 +80,9 @@ class Dataset(torch.utils.data.Dataset):
     def _load_raw_labels(self): # to be overridden by subclass
         raise NotImplementedError
 
+    def _load_raw_caption(self, sent_ix): # to be overridden by subclass
+        raise NotImplementedError
+
     def __getstate__(self):
         return dict(self.__dict__, _raw_labels=None)
 
@@ -90,6 +103,13 @@ class Dataset(torch.utils.data.Dataset):
         if self._xflip[idx]:
             assert image.ndim == 3 # CHW
             image = image[:, :, ::-1]
+
+        if self.name == 'birds' and self._use_labels:
+            sent_ix = np.random.randint(0, self.embeddings_num)
+            new_sent_ix = idx * self.embeddings_num + sent_ix
+            caps, cap_len = self._load_raw_caption(new_sent_ix)
+            return image.copy(), caps
+        
         return image.copy(), self.get_label(idx)
 
     def get_label(self, idx):
@@ -235,33 +255,42 @@ class ImageFolderDataset(Dataset):
 
 #----------------------------------------------------------------------------
 
+TEXT_CAPTIONS_PER_IMAGE = 10
+TEXT_WORDS_NUM = 18
 class TextDataset(Dataset):
     def __init__(self,
-        path,                   # Path to directory or zip.
-        resolution      = None, # Ensure specific resolution, None = highest available.
+        path,                   # Path to directory.
+        size = 256,             # Image size to be resized.
         **super_kwargs,         # Additional arguments for the Dataset base class.
     ):
         self._path = path
-        self._zipfile = None
+        self._crop_size = (size, size)
+        self.embeddings_num = TEXT_CAPTIONS_PER_IMAGE #TODO: add cfg file of alternative
+        split='train'
 
         if os.path.isdir(self._path):
             self._type = 'dir'
-            self._all_fnames = {os.path.relpath(os.path.join(root, fname), start=self._path) for root, _dirs, files in os.walk(self._path) for fname in files}
-        elif self._file_ext(self._path) == '.zip':
-            self._type = 'zip'
-            self._all_fnames = set(self._get_zipfile().namelist())
         else:
-            raise IOError('Path must point to a directory or zip')
+            raise IOError('Path must point to a directory')
 
         PIL.Image.init()
-        self._image_fnames = sorted(fname for fname in self._all_fnames if self._file_ext(fname) in PIL.Image.EXTENSION)
+
+        self.data_dir = path
+        if self.data_dir.find('birds') != -1:
+            self.bbox = self.load_bbox()
+        else:
+            self.bbox = None
+        split_dir = os.path.join(self.data_dir, split)
+
+        self._image_fnames, self.captions, self.ixtoword, \
+            self.wordtoix, self.n_words = self.load_text_data(self.data_dir, split)
         if len(self._image_fnames) == 0:
             raise IOError('No image files found in the specified path')
 
         name = os.path.splitext(os.path.basename(self._path))[0]
-        raw_shape = [len(self._image_fnames)] + list(self._load_raw_image(0).shape)
-        if resolution is not None and (raw_shape[2] != resolution or raw_shape[3] != resolution):
-            raise IOError('Image files do not match the specified resolution')
+        raw_shape = [len(self._image_fnames)] + [self._load_raw_image(0).shape[0], size, size]
+        self.class_id = self.load_class_id(split_dir, len(self._image_fnames))
+
         super().__init__(name=name, raw_shape=raw_shape, **super_kwargs)
 
     @staticmethod
@@ -292,29 +321,205 @@ class TextDataset(Dataset):
         return dict(super().__getstate__(), _zipfile=None)
 
     def _load_raw_image(self, raw_idx):
-        fname = self._image_fnames[raw_idx]
-        with self._open_file(fname) as f:
-            if pyspng is not None and self._file_ext(fname) == '.png':
-                image = pyspng.load(f.read())
-            else:
-                image = np.array(PIL.Image.open(f))
+        key = self._image_fnames[raw_idx]
+        
+        if self.bbox is not None:
+            bbox = self.bbox[key]
+            data_dir = '%s/CUB_200_2011' % self.data_dir
+        else:
+            bbox = None
+            data_dir = self.data_dir
+        
+        img_name = '%s/images/%s.jpg' % (data_dir, key)
+        img = PIL.Image.open(img_name).convert('RGB')
+        width, height = img.size
+        if bbox is not None:
+            r = int(np.maximum(bbox[2], bbox[3]) * 0.75)
+            center_x = int((2 * bbox[0] + bbox[2]) / 2)
+            center_y = int((2 * bbox[1] + bbox[3]) / 2)
+            y1 = np.maximum(0, center_y - r)
+            y2 = np.minimum(height, center_y + r)
+            x1 = np.maximum(0, center_x - r)
+            x2 = np.minimum(width, center_x + r)
+            img = img.crop([x1, y1, x2, y2])
+
+        image = np.array(img.resize(self._crop_size))
         if image.ndim == 2:
             image = image[:, :, np.newaxis] # HW => HWC
         image = image.transpose(2, 0, 1) # HWC => CHW
         return image
 
-    def _load_raw_labels(self):
-        fname = 'dataset.json'
-        if fname not in self._all_fnames:
-            return None
-        with self._open_file(fname) as f:
-            labels = json.load(f)['labels']
-        if labels is None:
-            return None
-        labels = dict(labels)
-        labels = [labels[fname.replace('\\', '/')] for fname in self._image_fnames]
-        labels = np.array(labels)
-        labels = labels.astype({1: np.int64, 2: np.float32}[labels.ndim])
-        return labels
+    def _load_raw_caption(self, sent_ix):
+        # a list of indices for a sentence
+        sent_caption = np.asarray(self.captions[sent_ix]).astype('int64')
+        if (sent_caption == 0).sum() > 0:
+            print('ERROR: do not need END (0) token', sent_caption)
+        num_words = len(sent_caption)
+        # pad with 0s (i.e., '<end>')
+        x = np.zeros((TEXT_WORDS_NUM, 1), dtype='int64')
+        x_len = num_words
+        if num_words <= TEXT_WORDS_NUM:
+            x[:num_words, 0] = sent_caption
+        else:
+            ix = list(np.arange(num_words))  # 1, 2, 3,..., maxNum
+            np.random.shuffle(ix)
+            ix = ix[:TEXT_WORDS_NUM]
+            ix = np.sort(ix)
+            x[:, 0] = sent_caption[ix]
+            x_len = TEXT_WORDS_NUM
+        return x, x_len
+
+    # def _load_raw_labels(self):
+    #     sent_ix = np.random.randint(0, self.embeddings_num, self._raw_shape[0])
+    #     new_sent_ix = np.arange(self._raw_shape[0]).astype('int64') * self.embeddings_num + sent_ix
+    #     labels, label_len = self.get_caption(new_sent_ix)
+    #     return labels
+    
+    def load_bbox(self):
+        data_dir = self.data_dir
+        bbox_path = os.path.join(data_dir, 'CUB_200_2011/bounding_boxes.txt')
+        df_bounding_boxes = pd.read_csv(bbox_path,
+                                        delim_whitespace=True,
+                                        header=None).astype(int)
+        
+        filepath = os.path.join(data_dir, 'CUB_200_2011/images.txt')
+        df_filenames = \
+            pd.read_csv(filepath, delim_whitespace=True, header=None)
+        filenames = df_filenames[1].tolist()
+        print('Total filenames: ', len(filenames), filenames[0])
+        
+        filename_bbox = {img_file[:-4]: [] for img_file in filenames}
+        numImgs = len(filenames)
+        for i in range(0, numImgs):
+            # bbox = [x-left, y-top, width, height]
+            bbox = df_bounding_boxes.iloc[i][1:].tolist()
+
+            key = filenames[i][:-4]
+            filename_bbox[key] = bbox
+        
+        return filename_bbox
+    
+    def load_captions(self, data_dir, filenames):
+        all_captions = []
+        for i in range(len(filenames)):
+            cap_path = '%s/text/%s.txt' % (data_dir, filenames[i])
+            with open(cap_path, "r") as f:
+                captions = f.read().decode('utf8').split('\n')
+                cnt = 0
+                for cap in captions:
+                    if len(cap) == 0:
+                        continue
+                    cap = cap.replace("\ufffd\ufffd", " ")
+                    # picks out sequences of alphanumeric characters as tokens
+                    # and drops everything else
+                    tokenizer = RegexpTokenizer(r'\w+')
+                    tokens = tokenizer.tokenize(cap.lower())
+                    if len(tokens) == 0:
+                        print('cap', cap)
+                        continue
+
+                    tokens_new = []
+                    for t in tokens:
+                        t = t.encode('ascii', 'ignore').decode('ascii')
+                        if len(t) > 0:
+                            tokens_new.append(t)
+                    all_captions.append(tokens_new)
+                    cnt += 1
+                    if cnt == self.embeddings_num:
+                        break
+                if cnt < self.embeddings_num:
+                    print('ERROR: the captions for %s less than %d'
+                          % (filenames[i], cnt))
+        return all_captions
+
+    def build_dictionary(self, train_captions, test_captions):
+        word_counts = defaultdict(float)
+        captions = train_captions + test_captions
+        for sent in captions:
+            for word in sent:
+                word_counts[word] += 1
+
+        vocab = [w for w in word_counts if word_counts[w] >= 0]
+
+        ixtoword = {}
+        ixtoword[0] = '<end>'
+        wordtoix = {}
+        wordtoix['<end>'] = 0
+        ix = 1
+        for w in vocab:
+            wordtoix[w] = ix
+            ixtoword[ix] = w
+            ix += 1
+
+        train_captions_new = []
+        for t in train_captions:
+            rev = []
+            for w in t:
+                if w in wordtoix:
+                    rev.append(wordtoix[w])
+            # rev.append(0)  # do not need '<end>' token
+            train_captions_new.append(rev)
+
+        test_captions_new = []
+        for t in test_captions:
+            rev = []
+            for w in t:
+                if w in wordtoix:
+                    rev.append(wordtoix[w])
+            # rev.append(0)  # do not need '<end>' token
+            test_captions_new.append(rev)
+
+        return [train_captions_new, test_captions_new,
+                ixtoword, wordtoix, len(ixtoword)]
+
+    def load_text_data(self, data_dir, split):
+        filepath = os.path.join(data_dir, 'captions.pickle')
+        train_names = self.load_filenames(data_dir, 'train')
+        test_names = self.load_filenames(data_dir, 'test')
+        if not os.path.isfile(filepath):
+            train_captions = self.load_captions(data_dir, train_names)
+            test_captions = self.load_captions(data_dir, test_names)
+
+            train_captions, test_captions, ixtoword, wordtoix, n_words = \
+                self.build_dictionary(train_captions, test_captions)
+            with open(filepath, 'wb') as f:
+                pickle.dump([train_captions, test_captions,
+                             ixtoword, wordtoix], f, protocol=2)
+                print('Save to: ', filepath)
+        else:
+            with open(filepath, 'rb') as f:
+                x = pickle.load(f)
+                train_captions, test_captions = x[0], x[1]
+                ixtoword, wordtoix = x[2], x[3]
+                del x
+                n_words = len(ixtoword)
+                print('Load from: ', filepath)
+        if split == 'train':
+            # a list of list: each list contains
+            # the indices of words in a sentence
+            captions = train_captions
+            filenames = train_names
+        else:  # split=='test'
+            captions = test_captions
+            filenames = test_names
+        return filenames, captions, ixtoword, wordtoix, n_words
+
+    def load_class_id(self, data_dir, total_num):
+        if os.path.isfile(data_dir + '/class_info.pickle'):
+            with open(data_dir + '/class_info.pickle', 'rb') as f:
+                class_id = pickle.load(f, encoding='latin1') # ? encoding solves a decoding problem
+        else:
+            class_id = np.arange(total_num)
+        return class_id
+
+    def load_filenames(self, data_dir, split):
+        filepath = '%s/%s/filenames.pickle' % (data_dir, split)
+        if os.path.isfile(filepath):
+            with open(filepath, 'rb') as f:
+                filenames = pickle.load(f)
+            print('Load filenames from: %s (%d)' % (filepath, len(filenames)))
+        else:
+            filenames = []
+        return filenames
 
 #----------------------------------------------------------------------------
