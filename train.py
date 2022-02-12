aee607d373488644c318555bb57057b8cb944ff9
Ismail Shaheen <i.shaheen@aucegypt.edu>
2022-02-12 19:28:43 +0200

    error to account for label lenght in metrics fixed

--- train.py
+++ train.py
@@ -8,6 +8,8 @@
 
 """Train a GAN using the techniques described in the paper
 "Training Generative Adversarial Networks with Limited Data"."""
+import warnings
+warnings.filterwarnings("ignore")
 
 import os
 import click
